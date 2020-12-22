b145386a76db19d523a782e6b2a243fc33fc831e
Albert Schotschneider <schotschneider@gmail.com>
2020-12-22 19:11:49 +0100

    Buff after revive in dungeon.

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 'use strict'
 
+function buff(mod, player_location) {
+    setTimeout(
+        () => {
+            mod.send('C_START_SKILL', 7, {
+                skill: {
+                    id: 450100,
+                    reserved: 0,
+                    npc: false,
+                    type: 1,
+                    huntingZoneId: 0
+                },
+                w: player_location.w,
+                loc: player_location.loc,
+                dest: player_location.loc,
+                unk: true,
+                moving: false,
+                continue: false,
+                target: 0,
+                unk2: false
+            });
+        }, 1000);
+}
+
 module.exports = function MysticAutoBuff(mod) {
     let player_location = {};
     let sent = false;
-
-
-    mod.hook('S_SPAWN_ME', 3, (event) => {
-        player_location.loc = event.loc;
-        player_location.w = event.w;
-    });
 
     mod.hook('C_PLAYER_LOCATION', 5, (event) => {
         player_location.loc = event.loc;
         player_location.w = event.w;
     });
-    
+
+    mod.hook('C_REVIVE_NOW', 2, (event) => {
+        buff(mod, player_location);
+    });
+
     mod.hook('S_SPAWN_ME', 3, (event) => {
-        setTimeout(
-            () => {
-                mod.send('C_START_SKILL', 7, {
-                    skill: {
-                        id: 450100,
-                        reserved: 0,
-                        npc: false,
-                        type: 1,
-                        huntingZoneId: 0
-                    },
-                    w: player_location.w,
-                    loc: player_location.loc,
-                    dest: player_location.loc,
-                    unk: true,
-                    moving: false,
-                    continue: false,
-                    target: 0,
-                    unk2: false
-                });
-            }, 1000);
+        player_location.loc = event.loc;
+        player_location.w = event.w;
+        buff(mod, player_location);
     });
 }
 
