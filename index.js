bd4a13c901732b29a6bff2d91e0a53df522edf41
Albert Schotschneider <schotschneider@gmail.com>
2020-12-22 19:19:02 +0100

    Refactor.

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 'use strict'
 
-function buff(mod, player_location) {
+
+/**
+ * Creates a skill object with the specified skill id and the location of the player.
+ * @param skill_id
+ * @param player_location
+ * @returns {{loc: *, unk: boolean, continue: boolean, unk2: boolean, skill: {npc: boolean, reserved: number, huntingZoneId: number, id, type: number}, w: *, dest: *, moving: boolean, target: number}}
+ */
+function create_skill(skill_id, player_location) {
+    return {
+        skill: {
+            id: skill_id,
+            reserved: 0,
+            npc: false,
+            type: 1,
+            huntingZoneId: 0
+        },
+        w: player_location.w,
+        loc: player_location.loc,
+        dest: player_location.loc,
+        unk: true,
+        moving: false,
+        continue: false,
+        target: 0,
+        unk2: false
+    }
+}
+
+
+/**
+ * Buffs the player after a given amount of delay in milliseconds.
+ *
+ * @param mod The TERA ToolBox module.
+ * @param player_location The current player location as def `C_PLAYER_LOCATION`.
+ * @param delay_ms The delay in ms after which the buff should be activated.
+ */
+function buff(mod, player_location, delay_ms) {
     setTimeout(
         () => {
-            mod.send('C_START_SKILL', 7, {
-                skill: {
-                    id: 450100,
-                    reserved: 0,
-                    npc: false,
-                    type: 1,
-                    huntingZoneId: 0
-                },
-                w: player_location.w,
-                loc: player_location.loc,
-                dest: player_location.loc,
-                unk: true,
-                moving: false,
-                continue: false,
-                target: 0,
-                unk2: false
-            });
-        }, 1000);
+            mod.send('C_START_SKILL', 7, create_skill(450100, player_location));
+        }, delay_ms);
 }
 
 module.exports = function MysticAutoBuff(mod) {
     let player_location = {};
-    let sent = false;
 
     mod.hook('C_PLAYER_LOCATION', 5, (event) => {
         player_location.loc = event.loc;
@@ -33,13 +51,13 @@ module.exports = function MysticAutoBuff(mod) {
     });
 
     mod.hook('C_REVIVE_NOW', 2, (event) => {
-        buff(mod, player_location);
+        buff(mod, player_location, 1000);
     });
 
     mod.hook('S_SPAWN_ME', 3, (event) => {
         player_location.loc = event.loc;
         player_location.w = event.w;
-        buff(mod, player_location);
+        buff(mod, player_location, 1000);
     });
 }
 
