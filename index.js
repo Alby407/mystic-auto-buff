57b772c1164772fae6ebdaadafd7e22333a784ef
Albert Schotschneider <schotschneider@gmail.com>
2021-02-03 16:09:11 +0100

    Sync.

--- index.js
+++ index.js
@@ -52,15 +52,15 @@ module.exports = function MysticAutoBuff(mod) {
 
     mod.hook('C_REVIVE_NOW', 2, (event) => {
         buff(mod, player_location, 1000, 450100);
-        buff(mod, player_location, 3000, 130400);
-        buff(mod, player_location, 4000, 160100);
+        buff(mod, player_location, 2000, 130400);
+        buff(mod, player_location, 5000, 160100);
     });
 
     mod.hook('S_SPAWN_ME', 3, (event) => {
         player_location.loc = event.loc;
         player_location.w = event.w;
         buff(mod, player_location, 1000, 450100);
-        buff(mod, player_location, 3000, 130400);
-        buff(mod, player_location, 4000, 160100);
+        buff(mod, player_location, 2000, 130400);
+        buff(mod, player_location, 5000, 160100);
     });
 }
